1a03bd386a074d969867b70a3b8a6a90844409a7
githrishii <114620588+githrishii@users.noreply.github.com>
2025-09-22 11:38:00 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
 
-echo "======================================="
-echo "########## Installing JAVA ##########"
-echo "======================================="
-
 sudo apt update
 sudo apt install fontconfig openjdk-21-jre -y
 java -version
 
-echo "======================================="
-echo "########## Installing Jenkins ##########"
-echo "======================================="
-
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | \
-  sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 sudo apt update
 sudo apt install jenkins -y
-
-echo "======================================="
-echo "########## Jenkins Installed Successfully ##########"
-echo "======================================="
 
 echo "You can access Jenkins using http://<your-server-ip>:8080"
 echo "To get the initial admin password, use the following command:"
